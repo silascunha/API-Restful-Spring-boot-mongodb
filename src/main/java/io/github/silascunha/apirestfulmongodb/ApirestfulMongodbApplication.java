0dc6aeac24f796927fd376380ca17829b0a4de03
Silas Fernandes da Cunha <silascunha09@hotmail.com>
2021-02-01 15:05:52 -0300

    Projeto criado

---
+++ src/main/java/io/github/silascunha/apirestfulmongodb/ApirestfulMongodbApplication.java
@@ -0,0 +1,13 @@
+package io.github.silascunha.apirestfulmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestfulMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestfulMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/silascunha/apirestfulmongodb/ApirestfulMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.silascunha.apirestfulmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestfulMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
